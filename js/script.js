3e7f7736aec324c96096a7d644a0b03e2501304b
BrigitteFlores16 <macalupufloresyiyibrigitte@gmail.com>
2024-10-29 18:49:09 +0100

    completato esercizio

--- js/script.js
+++ js/script.js
@@ -36,3 +36,23 @@ const teamMembers = [
     img: "img/female3.png"
   }
 ];
+function renderTeam() {
+const teamContainer = document.getElementById('team-members');
+teamMembers.forEach(member => {
+  const memberCard = `
+    <div class="col-md-4 mb-4">
+       <div class="card team-card d-flex flex-row">
+        <img src="./${member.img}" class="card-img-left" alt="${member.name}">
+         <div class="card-body bg-dark text-light d-flex flex-column justify-content-center">
+           <h3 class="card-title h5">${member.name}</h3>
+            <h4 class="card-text h6">${member.role}</h4>
+            <a href="mailto:${member.email}" class="text-info">${member.email}</a>
+            </div>
+         </div>
+      </div>
+  `;
+teamContainer.innerHTML += memberCard;
+  });
+}
+
+renderTeam();
